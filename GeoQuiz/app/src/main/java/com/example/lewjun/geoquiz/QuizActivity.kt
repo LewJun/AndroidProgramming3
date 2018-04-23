80c1e3b270d2ab590ddedb48ca7815edc6f23b56
LewJun <lewjun072@sina.com>
2018-04-23 13:45:38 +0800

    定制toast消息，改在屏幕顶部而不是底部显示弹出消息。`setGravity(Gravity.TOP)`

--- GeoQuiz/app/src/main/java/com/example/lewjun/geoquiz/QuizActivity.kt
+++ GeoQuiz/app/src/main/java/com/example/lewjun/geoquiz/QuizActivity.kt
@@ -1,7 +1,8 @@
 package com.example.lewjun.geoquiz
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
 
@@ -15,12 +16,20 @@ class QuizActivity : AppCompatActivity() {
 
         mTrueButton = findViewById(R.id.true_button)
         mTrueButton.setOnClickListener {
-            Toast.makeText(this@QuizActivity, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this@QuizActivity, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            // 通过设置gravity，改变toast的显示位置
+            val toast = Toast.makeText(this@QuizActivity, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
         }
 
         mFalseButton = findViewById(R.id.false_button)
         mFalseButton.setOnClickListener {
-            Toast.makeText(this@QuizActivity, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            // Toast.makeText(this@QuizActivity, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            // 通过设置gravity，改变toast的显示位置
+            val toast = Toast.makeText(this@QuizActivity, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
         }
     }
 }
