f0213f4ea98f2e4fbef5a8bd17e6eee8a2b99317
LewJun <lewjun072@sina.com>
2018-12-04 19:10:40 +0800

    补充缺少的两个类

---
+++ GeoQuiz/app/src/main/java/com/example/lewjun/geoquiz/EnumSex.java
@@ -0,0 +1,8 @@
+package com.example.lewjun.geoquiz;
+
+/**
+ * Created by LewJun on 2018/12/04.
+ */
+public enum EnumSex {
+    MALE, FEMALE
+}
---
+++ GeoQuiz/app/src/main/java/com/example/lewjun/geoquiz/UserInfo.kt
@@ -0,0 +1,16 @@
+package com.example.lewjun.geoquiz
+
+/**
+ * Created by LewJun on 2018/12/04.
+ */
+class UserInfo(
+        val id: Long? = null,
+
+        val username: String? = null,
+
+        val password: String? = null,
+
+        val sex: EnumSex? = null,
+
+        val age: Int? = null
+)
